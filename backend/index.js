344ae8ca277c1e377eae9cec3ec9f91796259064
Saman <pradhansaman2017@gmail.com>
2023-05-07 08:04:07 +0530

    new commit for backend

---
+++ backend/index.js
@@ -0,0 +1,68 @@
+//importing dependencing
+
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { connection } = require("./config/db");
+const { userRouter } = require("./routes/user.route");
+const { productRouter } = require("./routes/product.route");
+const { orderRouter } = require("./routes/order.route");
+const { passport } = require("./config/google_oauth");
+const { authentication } = require("./middlewares/auth.middleware");
+
+const cookieparser = require("cookie-parser");
+
+const PORT = process.env.PORT;
+//starting the express app
+const app = express();
+app.use(cookieparser());
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+  res.status(200).send({ msg: "home route" });
+});
+
+//google oauth route
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: [
+      "profile",
+      "email",
+      // "https://www.googleapis.com/auth/user.phonenumbers.read",
+    ],
+  })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/login",
+    session: false,
+  }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    // console.log(req);
+    res.redirect("/");
+    res.cooki;
+  }
+);
+//main routes
+app.use("/users", userRouter);
+
+//authentication
+app.use(authentication);
+
+app.use("/products", productRouter);
+app.use("/orders", orderRouter);
+
+//listening to server
+app.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log("connected to db");
+  } catch (error) {
+    console.log("can't connect to db");
+  }
+  console.log("server is running at " + PORT);
+});
---
+++ backend/routes/product.route.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const productRouter = express.Router();
+const { productModel } = require("../models/product.model");
+const { CommentModel } = require("../models/comment.model");
+
+//adding products to DB
+productRouter.post("/add", async (req, res) => {
+  console.log(req.body);
+  const { image1, image2, name, brand, size, gender, price } = req.body;
+  try {
+    const newProduct = new productModel(req.body);
+    await newProduct.save();
+    res.status(200).send({ msg: "Post is added" });
+  } catch (error) {
+    res.status(400).send({ msg: error.message });
+  }
+});
+
+//getting all the data of products from DB
+productRouter.get("/", async (req, res) => {
+  try {
+    const getProduct = await productModel.find();
+    console.log(getProduct);
+    res.status(200).send(getProduct);
+  } catch (error) {
+    res.status(400).send({ msg: error.message });
+  }
+});
+
+//updating the products
+
+productRouter.patch("/update/:id", async (req, res) => {
+  let id = req.params.id;
+  console.log(req.body, id);
+  try {
+    await productModel.findByIdAndUpdate(id, req.body);
+
+    res.status(200).send({ msg: "Product is update" });
+  } catch (error) {
+    res.status(400).send({ msg: error.message });
+  }
+});
+
+//deleting the products
+productRouter.delete("/delete/:id", async (req, res) => {
+  let id = req.params.id;
+  try {
+    await productModel.findByIdAndDelete({ _id: id });
+
+    res.status(200).send({ msg: "Product is deleted" });
+  } catch (error) {
+    res.status(400).send({ msg: error.message });
+  }
+});
+
+
+// add comments
+productRouter.post("/comment/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { userid, msg } = req.body;
+    const payload = {
+      userid,
+      msg,
+      productid: id
+    }
+    const comment = new CommentModel(payload);
+    await comment.save();
+
+    res.status(200).send({ msg: "comment is saved" });
+  } catch (error) {
+    res.status(400).send({ msg: error.message });
+  }
+});
+
+productRouter.get("/comments/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await CommentModel.find({ productid: id });
+
+    res.status(200).send({ data });
+  } catch (error) {
+    res.status(400).send({ msg: error.message });
+  }
+});
+module.exports = { productRouter };
