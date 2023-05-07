3555175aaa9f29fb4f0938752223c21272576c97
Saman <pradhansaman2017@gmail.com>
2023-05-07 21:22:43 +0530

    new  5"

--- backend/models/blacklist.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const blacklistSchema = mongoose.Schema({
-  token: String,
-});
-
-const blacklistModel = mongoose.model("backlistedToken", blacklistSchema);
-module.exports = { blacklistModel };
